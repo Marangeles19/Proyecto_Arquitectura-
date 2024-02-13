7c346bcccb6ee5dedd82ccc379b99d9eadcad845
lgonzalez <luisgx15@gmail.com>
2024-02-12 20:18:17 -0500

    eureka-service

--- inventario-service/src/main/java/com/microservice/inventario/InventarioServiceApplication.java
+++ inventario-service/src/main/java/com/microservice/inventario/InventarioServiceApplication.java
@@ -2,12 +2,14 @@ package com.microservice.inventario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class GestionInventarioApplication {
+@EnableDiscoveryClient
+public class InventarioServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GestionInventarioApplication.class, args);
+        SpringApplication.run(InventarioServiceApplication.class, args);
     }
 
 }
